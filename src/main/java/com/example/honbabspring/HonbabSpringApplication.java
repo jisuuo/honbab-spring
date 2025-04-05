d10e664cccef369442c5fd534614813d968f0a97
jissuo <jisu2298@gmail.com>
2025-04-05 10:32:15 +0900

    first commit

---
+++ src/main/java/com/example/honbabspring/HonbabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.honbabspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonbabSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HonbabSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/honbabspring/HonbabSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.honbabspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonbabSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
