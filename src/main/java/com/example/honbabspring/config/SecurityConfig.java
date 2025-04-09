bd0397e0c454ae315f41b6356a53a77dbcd665bb
jissuo <jisu2298@gmail.com>
2025-04-09 21:50:46 +0900

    20250409 1. loginFilter attemptAuthentication 추가

--- src/main/java/com/example/honbabspring/config/SecurityConfig.java
+++ src/main/java/com/example/honbabspring/config/SecurityConfig.java
@@ -1,21 +1,38 @@
 package com.example.honbabspring.config;
 
+import com.example.honbabspring.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+
     }
 
     @Bean
@@ -33,6 +50,8 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll() // 모두 허가
                         .requestMatchers("/admin").hasRole("ADMIN")              // admin인 경우만 admin 페이지 허가
                         .anyRequest().authenticated());
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         http
                 .sessionManagement((session) -> session
