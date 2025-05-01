446e328818c7eead552a6aa947f16af80f23cff8
jissuo <jisu2298@gmail.com>
2025-05-01 13:12:25 +0900

    대댓글 CRUD 구현

---
+++ src/main/java/com/example/honbabspring/controller/CommentController.java
@@ -0,0 +1,28 @@
+package com.example.honbabspring.controller;
+
+import com.example.honbabspring.dto.CommentCreateRequest;
+import com.example.honbabspring.dto.CommentResponse;
+import com.example.honbabspring.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(@PathVariable("commentId") Long commentId) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequest request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v1/comments/{commentId}")
+    public void delete(@PathVariable("commentId") Long commentId) {
+        commentService.delete(commentId);
+    }
+}
